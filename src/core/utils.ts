e1f1a1ff9fae07a3117090bb31b809ca0e1bbe66
TeoryMan <jonathancortestm143@gmail.com>
2024-01-20 16:24:28 -0500

    refactor: rename 'args' to 'classes'

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,6 +1,6 @@
 import { twMerge } from 'tailwind-merge';
 import { clsx, ClassValue } from 'clsx';
 
-export function cn(...args: ClassValue[]) {
-  return twMerge(clsx(args));
+export function cn(...classes: ClassValue[]) {
+  return twMerge(clsx(classes));
 }
