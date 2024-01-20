1fd81d5fe54077d89b51e782d1f488d3011bc4b3
TeoryMan <jonathancortestm143@gmail.com>
2024-01-20 17:18:57 -0500

    feat: user number column

--- src/core/db/tables.ts
+++ src/core/db/tables.ts
@@ -8,6 +8,7 @@ export const users = pgTable(
     name: text('name').notNull(),
     email: text('email').notNull().unique(),
     password: text('password').notNull(),
+    number: text('number').notNull(),
   },
   users => {
     return {
