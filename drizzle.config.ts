35f21d35199f0e437f3069ceb48b1d491e0d161b
TeoryMan <jonathancortestm143@gmail.com>
2024-01-20 13:32:26 -0500

    chore: set up migrations

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'drizzle-kit';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.development.local' });
+
+export default {
+  out: './drizzle',
+  schema: './src/core/db/tables.ts',
+  driver: 'pg',
+  dbCredentials: {
+    connectionString: process.env.DG_URL! + '?sslmode=require',
+  },
+  verbose: true,
+  strict: true,
+} satisfies Config;
