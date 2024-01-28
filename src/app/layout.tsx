4ccc4f58e04b14e622d671bd82cb1bf8ffbe9710
TeoryMan <jonathancortestm143@gmail.com>
2024-01-28 10:04:45 -0500

    refactor: remove unnecesary imports and async function

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,13 @@
 import type { Metadata } from 'next';
 import { openSans } from '@/core/fonts';
 import './globals.css';
-import { auth } from '../../auth';
-import { RedirectType, redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: 'cataLOG',
   description: 'Una herramienta para vender tus productos.',
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
