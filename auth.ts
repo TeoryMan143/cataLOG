eacf113734ad6062855b7cdc8320b1a10003ccf8
TeoryMan <jonathancortestm143@gmail.com>
2024-02-11 17:42:46 -0500

    log 2

--- auth.ts
+++ auth.ts
@@ -33,6 +33,7 @@ export const handler = NextAuth({
   ],
   callbacks: {
     async signIn({ profile, account }) {
+      console.log(profile);
       if (account?.provider === 'google') {
         if (!profile) return false;
         const user = await getUserByEmail(profile.email!);
