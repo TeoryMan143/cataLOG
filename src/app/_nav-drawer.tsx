f279c16fb6821d178b17bedc8c783002ab5f5055
TeoryMan <jonathancortestm143@gmail.com>
2024-02-12 15:34:33 -0500

    TODO: drawer

--- src/app/_nav-drawer.tsx
+++ src/app/_nav-drawer.tsx
@@ -1,14 +1,35 @@
 'use client';
 
+import { cn } from '@/core/utils';
 import { Icon } from '@iconify/react';
+import { useRef, useState } from 'react';
 
 function NavDrawer() {
+  const [active, setActive] = useState(false);
+  const navRef = useRef<HTMLDivElement>(null);
+
   return (
     <>
-      <button className='text-2xl'>
+      <button className='text-2xl' onClick={() => setActive(!active)}>
         <Icon icon='fa6-solid:bars' />
       </button>
-      <nav className='hidden'>NavDrawer</nav>
+      <div
+        className={cn(
+          `absolute w-screen h-screen bg-gray-500/45 hidden justify-end`,
+          {
+            flex: active,
+          }
+        )}
+      >
+        <nav
+          ref={navRef}
+          className={cn(`w-72 h-full bg-black/80 relative translate-x-full`, {
+            'translate-x-0': active,
+          })}
+        >
+          NavDrawer
+        </nav>
+      </div>
     </>
   );
 }
