bff49ecb4b91406a7d169dfe9a5ba03a6dec0074
TeoryMan <jonathancortestm143@gmail.com>
2024-02-22 19:57:54 -0500

    feat: user svg icon

--- src/app/_page-components/nav-drawer/nav-ui.tsx
+++ src/app/_page-components/nav-drawer/nav-ui.tsx
@@ -1,11 +1,9 @@
-'use client';
-
 import { cn } from '@/core/client-utils';
 import { workSans } from '@/core/fonts';
 import { forwardRef } from 'react';
 import NavLinks from '../nav-links';
 import { useSession } from 'next-auth/react';
-import { Icon } from '@iconify/react';
+import { UserCircleIcon } from '@/components/icons/user-circle';
 
 type Props = React.ComponentPropsWithRef<'div'> & {
   active: boolean;
@@ -43,7 +41,7 @@ const NavUi = forwardRef<HTMLDivElement, Props>(function NavUi(
           hidden: !active && fixed,
         })}
       >
-        <Icon icon='ph:user-circle-fill' className='text-[9rem]' />
+        <UserCircleIcon className='text-[9rem]' />
         <p className={cn(`text-xl`, workSans.className)}>{user?.name}</p>
       </div>
       <nav>
