1084aea20c23cb7ece371b33850f75559ea58a39
TeoryMan <jonathancortestm143@gmail.com>
2024-04-18 15:01:40 -0500

    TODO: cart-item

--- src/app/(main)/cart/_page-components/cart-item.tsx
+++ src/app/(main)/cart/_page-components/cart-item.tsx
@@ -1,8 +1,14 @@
-function CartItem() {
+import { getProductImages } from '@/core/lib/db/products';
+import { DBCartItem } from '@/core/schemas/cart';
+import Image from 'next/image';
+
+async function CartItem({ item }: { item: DBCartItem }) {
+  const image = item.productId ? await getProductImages(item.productId) : null;
+
   return (
     <div>
-      
+      <Image src='/product-default.png' alt={item.displayName} />
     </div>
-  )
+  );
 }
-export default CartItem
+export default CartItem;
