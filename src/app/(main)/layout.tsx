3ebac9ae1f20c92f1af341c948d32e1d7d946c9e
TeoryMan <jonathancortestm143@gmail.com>
2024-04-28 22:30:33 -0500

    feat: title

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -5,6 +5,7 @@ import styles from '../page.module.css';
 import { cn } from '@/core/client-utils';
 import AsideNav from '../_page-components/aside-nav';
 import Link from 'next/link';
+import { workSans } from '@/core/fonts';
 
 export default async function Home({
   children,
@@ -21,11 +22,19 @@ export default async function Home({
       <header
         className={cn('bg-black text-white py-4 px-4', styles.headerArea)}
       >
-        <div className='flex justify-around items-center gap-5'>
+        <div className='flex justify-between items-center gap-5'>
           <Link href='/' className='text-4xl'>
             <Logo />
           </Link>
-          <SearchBar />
+          {/* <SearchBar /> */}
+          <h1
+            className={cn(
+              'font-bold text-3xl text-white text-center flex-1',
+              workSans.className,
+            )}
+          >
+            CataLOG
+          </h1>
           <NavDrawer />
         </div>
       </header>
